9ad0b249f0135ffc9a3acd5f34f8296fb13407ea
6uiwj <oterges00@gmail.com>
2025-01-15 01:39:32 +0900

    250115Array

---
+++ app.js
@@ -0,0 +1,33 @@
+// const a = 5; //변수 생성
+// const b = 2;
+// //변수 작성 규칙(camel case)
+// //  공백 불가능, 띄어쓰기는 대문자로 구분
+// let myName = "ming";
+// console.log(a, b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello ", myName);
+
+// myName = "minji";
+
+// console.log("your name is ", myName);
+
+// //#2.4 Booleans(250114)
+// const amiFat = null;
+// let something;
+// console.log(amiFat, something);
+
+//#2.5 Arrays(250114)
+console.log("--#2.5 Arrays(250114)--");
+
+//Array 기호 : [] 사용
+const daysOfWeek = ["mon", "thu", "wed", "thu", "fri", "sat"];
+// console.log(daysOfWeek);
+
+// //Add one more day to the array
+// daysOfWeek.push("sun");
+
+// console.log(daysOfWeek);
+
+//Array에 담긴 요소의 개수를 넘은 숫자를 호출할 경우
+console.log(daysOfWeek[8]);
