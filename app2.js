55bd9439b0c273dc02af9b829dd3884480fd140d
6uiwj <oterges00@gmail.com>
2025-01-23 00:30:43 +0900

    250122 document

--- app2.js
+++ app2.js
@@ -2,9 +2,20 @@
 //object
 //document : 내가 작성한 HTML을 가져올 수 있다.
 
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 
-//console.dir(title);
-title.innerText = "Got you!";
-console.log(title.id);
-console.log(title.className);
+// //console.dir(title);
+// title.innerText = "Got you!";
+// //console.log(title.id);
+// console.log(title.className);
+
+//#3.2
+// const hellos = document.getElementsByClassName("hello");
+// console.log(hellos);
+
+//const title = document.getElementsByTagName("h1");
+// const title = document.querySelectorAll(".hello h1");
+// console.log(title);
+
+const title = document.querySelector("#hello");
+console.log(title);
