768e14dafd641ad350bd1b84065f7e50615321df
6uiwj <oterges00@gmail.com>
2025-01-29 23:08:07 +0900

    250129 recaf

--- app2.js
+++ app2.js
@@ -17,6 +17,46 @@
 // const title = document.querySelectorAll(".hello h1");
 // console.log(title);
 
-const title = document.querySelector("#hello");
-console.log(title);
+//const title = document.querySelector("#hello");
+//console.log(title);
 //const title = document.getElementsByTagName("h1");
+function plus(a, b) {
+  console.log(a + b);
+}
+
+function minus(a, b) {
+  console.log(a - b);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+function divide(a, b) {
+  console.log(a / b);
+}
+
+function power(a, b) {
+  console.log(a ** b);
+}
+
+const cal = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiply: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    if (b === 0) {
+      console.log("can't divide");
+    } else {
+      console.log(a / b);
+    }
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
