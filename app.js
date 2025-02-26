d875b6175d4b1f26a84634b835fe175923c2741c
6uiwj <oterges00@gmail.com>
2025-02-26 17:07:47 +0900

    250226 login

--- app.js
+++ app.js
@@ -32,20 +32,41 @@
 
 // loginForm.addEventListener("submit", onLoginSubmit);
 
-//#4.3
+//#4.3-4.4
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+// const link = document.querySelector("a");
+
+// function onLoginSubmit(event) {
+//   event.preventDefault();
+//   const username = loginInput.value;
+//   loginForm.classList.add("hidden");
+//   console.log(username);
+// }
+
+// function handleLinkClick(event) {
+//   event.preventDefault();
+//   console.dir(event);
+// }
+
+// loginForm.addEventListener("submit", onLoginSubmit);
+// link.addEventListener("click", handleLinkClick);
+
+//#4.4
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"; //일반적으로 string만 포함된 변수는 대문자로 표기기
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username", username); //로컬 스토리지에 username 저장
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
